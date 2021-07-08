18bdf90db4ece6f3a91b898d986e31ace44b55fd
kaewpiyarattong <krittaporn.tong@gmail.com>
2021-07-08 22:25:03 +0700

    Lab2-3

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            brand: 'SE 331',
+            // image: './assets/images/socks_green.jpg',
+            // inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
-            cart: 0
-
+            selectedVariant:0,
+            cart: 0,
+            onSale: true
         }
     },
     methods: {
@@ -20,6 +22,29 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index){
+            this.selectedVariant = index;
         }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        Onsale(){
+            if(this.variants[this.selectedVariant].quantity > 0 && this.onSale == true) {
+            return this.brand + ' ' + this.product  + ' ' + "is on sale"
+            }
+            else{
+                return this.brand + ' ' + this.product  + ' ' + "not sale"
+                }
+        }
+
     }
 })
